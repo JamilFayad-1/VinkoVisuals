4d61d494b8630d8514ba39ccbe87c6ff59eb6dee
Jamil Fayad <jamilfayad@hotmail.ca>
2025-04-09 14:57:27 -0400

    Added the second section to the landing page.

--- vinkovisualsclient/src/App.jsx
+++ vinkovisualsclient/src/App.jsx
@@ -3,16 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { memo } from 'react';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Header from './components/Header/Header';
+import { ReactLenis, useLenis } from 'lenis/react'
 
 const MemoizedHeader = memo(Header);
 
 function App() {
+
+  const lenis = useLenis(({ scroll }) => {
+  })
+
   return (
     <Router>
-      <MemoizedHeader />
-      <Routes>
-        <Route path="/" element={<LandingPage />} /> 
-      </Routes>
+      <ReactLenis root>
+        <MemoizedHeader />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </ReactLenis>
     </Router>
   );
 }
--- vinkovisualsclient/src/pages/LandingPage/LandingPage.jsx
+++ vinkovisualsclient/src/pages/LandingPage/LandingPage.jsx
@@ -10,17 +10,30 @@ function LandingPage() {
   const containerRef = useRef(null);
 
   return (
-    <div className='landing-page-container'>
+    <>
+      <div className='landing-page-container'>
 
-      <div className='lanyard-container'>
-        <Lanyard gravity={[0, -40, 0]} />
+        <div className='lanyard-container'>
+          <Lanyard gravity={[0, -40, 0]} />
+        </div>
+
+        <div className='landing-page-text-container'>
+          <h1>Vinko Visuals is a creative design and visual experience <span>agency</span></h1>
+        </div>
+
       </div>
 
-      <div className='landing-page-text-container'>
-        <h1>Vinko Visuals is a creative design and visual experience <span>agency</span></h1>
+      <div className='landing-page-text-container-2'>
+        <div className="curved-container">
+          <svg viewBox="0 0 100 25" className="curve" preserveAspectRatio="none">
+            <path d="M 0 10 Q 50 -10 100 10 L 100 100 L 0 100 Z" fill="#c4e0ef" />
+          </svg>
+        </div>
+        <div className='landing-page-text-container-2-content-wrapper'>
+
+        </div>
       </div>
-      
-    </div>
+    </>
   );
 }
 
